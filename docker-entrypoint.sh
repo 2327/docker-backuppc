78c09dae2d5f2051b8ba22247a3477bdf2bf2da8
Maxim Klyuev <klyuevmv@mos.ru>
2019-01-09 15:48:00 +0300

    small fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,47 +1,47 @@
 #!/usr/bin/env sh
 
 set -exuo pipefail
-trap  rm -f $DATA/log/status.pl DATA/log/status.pl.old EXIT
+trap "rm -f ${DATA}/log/status.pl ${DATA}/log/status.pl.old" EXIT
 
-if [[ -z "$(ls -A $CONFIG)" ]]; then
-  echo "Creating new default configurationi..."
-  mv $INIT_CONFIG/* $CONFIG
-  rm -rf $INIT_CONFIG
-  chown -R backuppc:backuppc $CONFIG
+if [[ -z "$(ls -A ${CONFIG})" ]]; then
+  echo "Creating new default configuration..."
+  mv ${INIT_CONFIG}/* ${CONFIG}
+  rm -rf ${INIT_CONFIG}
+  chown -R backuppc:backuppc ${CONFIG}
 fi
 
-if [[ -z "$(ls -A $DATA)" ]]; then
+if [[ -z "$(ls -A ${DATA})" ]]; then
   echo "Creating data-directory from skel..."
-  mv $INIT_DATA/* $DATA
-  rm -rf $INIT_DATA
-  mkdir -p $DATA/.ssh
-  echo "Creating a ssh keypairi..."
-  ssh-keygen -N '' -f $DATA/.ssh/id_rsa
-  chown -R backuppc:backuppc $DATA
-  chmod -R 0600 $DATA/.ssh/*
+  mv ${INIT_DATA}/* ${DATA}
+  rm -rf ${INIT_DATA}
+  mkdir -p ${DATA}/.ssh
+  echo "Creating a ssh keypair..."
+  ssh-keygen -N '' -f ${DATA}/.ssh/id_rsa
+  chown -R backuppc:backuppc ${DATA}
+  chmod -R 0600 ${DATA}/.ssh/*
 fi
 
-if [[ ! -f "$DATA/.msmtprc" ]] && [[ ! -z ${MAILSERVER} ]]; then
-  echo "generating $DATA/.msmtprc config"
-  cat > "$DATA/.msmtprc" << EOF
+if [[ ! -f "${DATA}/.msmtprc" ]] && [[ ! -z ${MAILSERVER} ]]; then
+  echo "generating ${DATA}/.msmtprc config"
+  cat > "${DATA}/.msmtprc" << EOF
   account mail_account
   tls on
   tls_starttls off
   tls_certcheck off
   tls_trust_file /etc/ssl/certs/ca-certificates.crt
   auth on
-  host $MAILSERVER
-  port $PORT
-  from $FROM
-  user $USER
-  password $PASSWORD
+  host ${MAILSERVER}
+  port ${PORT}
+  from ${FROM}
+  user ${USER}
+  password ${PASSWORD}
 
   defaults
   account mail_account
   account default: mail_account
 EOF
-  chown -R backuppc:backuppc $DATA/.msmtprc
-  chmod -R 0600 $DATA/.msmtprc
+  chown -R backuppc:backuppc ${DATA}/.msmtprc
+  chmod -R 0600 ${DATA}/.msmtprc
 fi
 
 /usr/local/bin/dumb-init -c /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
